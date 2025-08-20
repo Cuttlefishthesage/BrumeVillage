c88b7ffb96f26e4959cb6dcc3cf9205a69778b94
Cuttlefishthesage <cuttlefishthesage@gmail.com>
2025-08-20 13:29:45 +0100

    Add all stuff for brume village mod
    * bringingjakv4over
    
    * updates and improvements and tidying
    
    * Further feedback and fixes and tidying
    
    * additional fixes. include tidying up boulder, collision, carts, adding some extra orbs to even out the numbers. added new load boundary. fixed up some grass in oob.
    
    * mountainpassfix
    
    * Final changes, resetting testzone stuff, making race-ring mount-race-ring, updating luminars cart to use up to date and dedicated file.
    
    * load boundary display stuff and moving text
    
    * made audio loops
    
    * load boudnary adjustments, moving terrain from one level glb to another to accomodate
    
    * adjusted ring race and added return ramps
    
    * Added thumbnail, adjusted water heights, fixed transparent door, changed launcher dir and camera quats
    
    * whitespace fixing oops
    
    ---------
    
    Co-authored-by: Cuttlefish <dummyemail>

---
+++ custom_assets/jak1/levels/abandon/abandon.gd
@@ -0,0 +1,58 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("ABA.DGO"
+(
+ "static-screen.o"
+ "abandoned-obs.o"
+ "pathing-minecart.o"
+ "minecart.o"
+ ;;"snow-ball.o"
+ ;;"village-obs.o" TESTING CRASH
+ ;;"villagep-obs.o" TESTING CRASH
+ ;;"village1-part.o" TESTING CRASH
+ ;;"village1-part2.o" TESTING CRASH
+ ;;"village2-part.o" ;;TESTING CRASH
+ "village2-obs.o" ;;TESTING CRASH LOAD BOUNDARY
+ ;;"village2-part2.o" TESTING CRASH
+ ;;"village3-part.o" ;;TESTING CRASH
+ ;;"village3-obs.o" ;;TESTING CRASH
+ "jungle-obs.o"
+ "jungleb-obs.o"
+ "jungle-part.o"
+ "maincave-obs.o"
+ "maincave-part.o"
+ "target-ice.o"
+ "ogre-obs.o"
+ "beach-obs.o"
+ ;; "sunken-water.o" crashes coming from village2
+ ;; "dark-eco-pool.o" causes crashing coming from misty straight to here
+ "swamp-bat.o"
+ "cavecrystal-light.o"
+ ;;"driller-lurker.o" using custom driller now
+ "swamp-rat.o"
+ "swamp-rat-nest.o"
+ "tpage-398.go" ;; moved tpages to appropriate spot 
+ "tpage-400.go"
+ "tpage-399.go"
+ "tpage-401.go"
+ "tpage-1470.go"
+ "plat-ag.go"
+ "swamp-rat-ag.go"
+ "swamp-rat-nest-ag.go"
+ "babak-ag.go"
+ "driller-lurker-ag.go"
+ ;;"snow-obs.o"
+ "swamp-bat-ag.go"
+ "eggtop-ag.go"
+ "pontoonten-ag.go"
+ "water-anim-sunken-dark-eco-ag.go"
+ "water-anim-sunken-ag.go"
+ "shortcut-boulder-ag.go"
+ "ecoventrock-ag.go"
+ "kickrock-ag.go"
+ "eichar-ice+0-ag.go"
+ "minecartsteel-ag.go"
+ "abandon.go"
+ ))
---
+++ custom_assets/jak1/levels/brume/brume.gd
@@ -0,0 +1,54 @@
+;; DGO definition file for Awful Village level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("BRU.DGO"
+ (
+  "brume-obs.o"
+  "oracle.o"
+  "linear-plat.o"
+  "village-obs.o"
+  "villagep-obs.o"
+  "village1-part.o"
+  "village1-part2.o"
+  "village2-obs.o" ;; TESTING CRASH
+  "village3-part.o"
+  "village3-obs.o"
+  "jungle-obs.o"
+  ;;"misty-part.o"
+  ;;"steam-cap.o"
+  "sunken-elevator.o"
+  ;;"village2-part.o" TEST FLAMEPOT
+  ;; "village2-obs.o" TEST CRASH POST FLAMEPOT
+  ;;"village2-part2.o" TEST FLAMEPOT
+  ;;"fisher.o"
+  ;;"sculptor.o"
+  ;;"ogre-obs.o"
+  "tpage-398.go" 
+  "tpage-400.go"
+  "tpage-399.go"
+  "tpage-401.go"
+  "tpage-1470.go" ;; moved tpages to more appropriate location
+  "sunkencam-ag.go"
+  "village2cam-ag.go"
+  "junglecam-ag.go"
+  "plat-button-ag.go"
+  "shortcut-boulder-ag.go"
+  "muse-ag.go"
+  "fisher-ag.go" 
+  "sunken-elevator-ag.go"
+  "oracle-ag.go"
+  "steam-cap-ag.go"
+  "plat-ag.go"
+  "plat-eco-ag.go"
+  "test-actor-ag.go"
+  "test-actor2-ag.go"
+  "brumesteamcap-ag.go"
+  "hutlamp-ag.go"
+  "yellowsage-ag.go"
+  "ecovalve-ag.go"
+  "warpgate-ag.go" 
+  "warp-gate-switch-ag.go"
+  "brume.go"
+  )
+ )
